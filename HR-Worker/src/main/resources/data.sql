38cdda2d1c39869d3125be50b05a78b681c208a8
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-08-23 17:50:00 -0300

    Project HR-Payroll microservice created.

--- HR-Worker/src/main/resources/data.sql
+++ HR-Worker/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Darth Vader', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Luke Skywalker', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Han Solo', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Master Yoda', 250.0);
