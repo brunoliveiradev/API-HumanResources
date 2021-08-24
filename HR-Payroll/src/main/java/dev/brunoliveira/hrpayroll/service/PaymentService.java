fd314c8c8854b0d30f52cf7453904154fe3df71b
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-08-24 15:46:36 -0300

    Feign Client implementation in HR-Payroll

--- HR-Payroll/src/main/java/dev/brunoliveira/hrpayroll/service/PaymentService.java
+++ HR-Payroll/src/main/java/dev/brunoliveira/hrpayroll/service/PaymentService.java
@@ -2,30 +2,21 @@ package dev.brunoliveira.hrpayroll.service;
 
 import dev.brunoliveira.hrpayroll.entities.Payment;
 import dev.brunoliveira.hrpayroll.entities.Worker;
-import org.springframework.beans.factory.annotation.Value;
+import dev.brunoliveira.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    private final WorkerFeignClient workerFeignClient;
 
-    private final RestTemplate restTemplate;
-
-    public PaymentService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
 
     public Payment getPayment(Long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        // Como é do tipo ResponseEntity, precisa colocar .getBody() para capturar a resposta
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
