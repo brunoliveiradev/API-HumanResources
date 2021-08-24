1ab25683f147b454915cca0e9a5e96793ee6c152
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-08-24 11:45:05 -0300

    Rest Template implementation.

--- HR-Payroll/src/main/java/dev/brunoliveira/hrpayroll/service/PaymentService.java
+++ HR-Payroll/src/main/java/dev/brunoliveira/hrpayroll/service/PaymentService.java
@@ -1,12 +1,31 @@
 package dev.brunoliveira.hrpayroll.service;
 
 import dev.brunoliveira.hrpayroll.entities.Payment;
+import dev.brunoliveira.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private final RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(Long workerId, int days) {
-        return new Payment("Anakyn", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
